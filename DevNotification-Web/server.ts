ee5dbeea00385547b3b8081f8ab98240492224d8
lhrolim <luizhenrique.rolim@gmail.com>
2016-10-10 23:04:59 -0300

    adding react boilerplate web project

---
+++ DevNotification-Web/server.ts
@@ -0,0 +1,7 @@
+﻿import http = require('http');
+import Index = require("../DevNotification/typings/globals/node/index");
+var port = Index.process.env.port || 1337
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
