cf6bcdc00ea823246e67311b28095b7f7367e642
lhrolim <luizhenrique.rolim@gmail.com>
2016-11-17 23:56:10 -0200

    adding jwt support

--- DevNotification-Api/index.js
+++ DevNotification-Api/index.js
@@ -1,3 +1,7 @@
+// JWTs
+var jwt = require('express-jwt');
+const cors = require('cors');
+
 const config = require('./config/config');
 
 const express  = require('express');
@@ -14,6 +18,7 @@ const app  = express();
 
 require('./utils/promisify-all')['bluebird'](['mongoose','request','request-promise']);
 
+//TODO: check https://github.com/expressjs/errorhandler
 const mongooseErrorHandler = require('./utils/mongooseErrorHandler');
 
 require('./utils/stringutils');
@@ -25,7 +30,20 @@ mongoose.Promise = require('bluebird');
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('tiny'));
+
+
+var authenticate = jwt({
+  secret: new Buffer("oj8DLWEqvubHefqgIkeqJS30pfsTjQWpTCyheR6hIt0KWjRjjPG5wP02IkDLq6yu", 'base64'),
+  audience: "oFMSf9OHqjAWRzj5uHym4Ew8MC0MuAho"
+});
+
+
+app.use(authenticate);
+
+
+
 
 app.use('/', routes);
 app.use(mongooseErrorHandler);
